112b1da4094cf3429b518283a54cb81aa37eb275
ThunderRoid <thunderroid@gmail.com>
2019-12-08 22:26:39 -0800

    policy_iteration

--- main.py
+++ main.py
@@ -16,14 +16,8 @@ def main():
     grid_world.set_prob(0.7, 0, 0.15, 0.15)  # forward, backward, left, right
     grid_world.set_reward(0.02, -1, 1)  # state_white, state_red, state_green
 
-    print(grid_world.step((1, 0), GW.ACTION_EAST))
-    print(grid_world.step((3, 3), GW.ACTION_WEST))
-    print(grid_world.step((3, 3), GW.ACTION_NORTH))
-    print(grid_world.step((3, 3), GW.ACTION_SOUTH))
-    print(grid_world.get_reward((0, 0)))
-
     policy = OP(grid_world)
-    policy.policy_iteration()
+    print(policy.policy_iteration())
 
 
 if __name__ == "__main__":
--- optimal_policy.py
+++ optimal_policy.py
@@ -5,8 +5,8 @@ import math
 
 
 class OptimalPolicy:
-    max_iterations = 1000
-    discount_factor = 1.0
+    max_iterations = 100000
+    discount_factor = 0.9
     epsilon = 0.1
 
     def __init__(self, world):
@@ -17,11 +17,44 @@ class OptimalPolicy:
         # print([[0 for _ in range(row)] for _ in range(col)])
 
     def policy_iteration(self):
-        pass
+        col, row = self.world.n_states
+        policy = [[(None if self.world.cell_at(x, y) == GW.STATE_WHITE else self.world.get_random_action()) for x in range(row)] for y in range(col)]
+
+        n_iterations = 0
+        run = True
+        while run:
+            self.__policy_evaluation(policy)
+            n_iterations += 1
+
+            changes = False
+            for x in range(row):
+                for y in range(col):
+                    if self.world.cell_at(x, y) == GW.STATE_WHITE:
+                        new_max = None
+                        arg_max = None
+                        for action in GW.all_actions:
+                            _sum = 0
+                            possibilities = self.world.step((x, y), action)
+                            for a, s_t1, p in possibilities:
+                                _sum += p * self.value_table[s_t1[0]][s_t1[1]]
+                            if (new_max is None) or (_sum > new_max):
+                                arg_max = action
+                                new_max = _sum
+                        _sum = 0
+                        possibilities = self.world.step((x, y), policy[x][y])
+                        for a, s_t1, p in possibilities:
+                            _sum += p * self.value_table[s_t1[0]][s_t1[1]]
+                        if new_max > _sum:
+                            policy[x][y] = arg_max
+                            changes = True
+            run = changes
+            if n_iterations >= self.max_iterations:
+                run = False
+                print("Ended: max iterations limit exceeded!")
+        return n_iterations
 
     def __policy_evaluation(self, policy):
         n_iterations = 0
-        max_norm = 0
         for i in range(int(self.max_iterations)):
             n_iterations += 1
             max_norm = 0
@@ -34,16 +67,21 @@ class OptimalPolicy:
 
                     if self.world.cell_at(x, y) == GW.STATE_WHITE:
                         action = policy[x][y]
+
+                        if action is None:
+                            action = policy[x][y] = self.world.get_random_action()
+
                         possibilities = self.world.step((x, y), action)
                         for a, s_t1, p in possibilities:
                             new_value_table[x][y] += p * self.value_table[s_t1[0]][s_t1[1]]
                         new_value_table[x][y] *= self.discount_factor
                     max_norm = max(max_norm, abs(self.value_table[x][y] - new_value_table[x][y]))
             self.value_table = new_value_table
-            print(new_value_table)
             if max_norm <= self.epsilon * (1 - self.discount_factor) / self.discount_factor:
+                print("Policy evaluated in {} iterations.".format(n_iterations))
                 break
             elif n_iterations >= self.max_iterations:
+                print("Ended: max iterations limit exceeded!--")
                 break
 
     def __generate_empty_matrix(self):
